19173dd73a8991abc2816b9046cd98703d70cd17
Saviskar <saviskar2003@gmail.com>
2025-09-22 02:59:40 +0530

    fix: installed tailwindcss

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pet Haven - All Products</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="../src/output.css">
 </head>
 <body class="bg-white">
